d919d89b502bc90241eb4846e140dc71d86fc97e
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2024-01-26 17:53:22 +0300

    update

--- server/app.py
+++ server/app.py
@@ -46,7 +46,43 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch (self,id):
+        plant=Plant.query.filter_by(id=id).first().to_dict()
 
+        for attr in request.form:
+            setattr(plant,attr,request.form.get[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        
+        response_body={
+            "id":1,
+            "name":"Aloe",
+            "price": 11.50,
+            "is_in_stock": False
+        }
+
+        response=make_response(
+            jsonify(response_body), 
+            200
+        )
+        return response
+    
+    def delete(self,id):
+        plant=Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        plant_dict=plant.to_dict()
+        
+        response=make_response(
+            jsonify(plant_dict),
+            200
+        )
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
